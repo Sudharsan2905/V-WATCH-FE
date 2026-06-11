3907dc0406f24e9c7fcf04fd9e915e204af66e27
Sudharsan Senthil <sudharsan.softsuave@gmail.com>
2026-06-12 00:34:05 +0530

    Route update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["outsider-curtsy-concert.ngrok-free.app", "outsider-curtsy-concert.ngrok-free.dev"],
 };
 
 export default nextConfig;
