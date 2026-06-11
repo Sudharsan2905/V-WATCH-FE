ebd65214f111bf9921e2688fe3323c37248477c4
Jagadish Y <jagadishj323@gmail.com>
2026-06-11 13:08:58 +0530

    BI - Dashboards - Responsiveness fixes

--- src/components/common/BookADemo/index.tsx
+++ src/components/common/BookADemo/index.tsx
@@ -12,13 +12,12 @@ const BUTTON_STYLE: React.CSSProperties = {
 
 const BUTTON_CLASSES =
   "h-11 gap-[10px] rounded-full px-4 text-base shadow-[2px_5px_14px_rgba(79,148,104,0.60),0_6px_42px_rgba(38,124,153,0.40)]";
-  "h-11 gap-[10px] rounded-full px-4 text-base shadow-[2px_5px_14px_rgba(79,148,104,0.60),0_6px_42px_rgba(38,124,153,0.40)]";
 
 function ArrowBadge() {
   return (
     <span className="flex h-[25px] w-[25px] items-center justify-center rounded-full bg-white">
       <svg width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g clip-path="url(#clip0_386_936)">
+        <g clipPath="url(#clip0_386_936)">
           <path d="M9.54396 8.23273L9.35582 0.792485L1.91558 0.621445L1.93268 2.24633L6.89284 2.21212L0.153867 8.95109L1.21431 10.0115L7.93619 3.28967L7.90198 8.21562L9.54396 8.23273Z" fill="#52BAAC" />
         </g>
         <defs>
