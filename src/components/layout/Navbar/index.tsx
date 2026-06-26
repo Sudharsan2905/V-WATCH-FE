635395852c6dae6ce76eb46a7082890b5ea3b10f
Sudharsan Senthil <sudharsan.softsuave@gmail.com>
2026-06-26 11:16:03 +0530

    Remove the contractor complaince

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -145,12 +145,12 @@ const DROPDOWNS: Record<string, DropdownData> = {
         desc: "Latest trends and platform updates.",
         href: "#",
       },
-      {
-        icon: "/nav/icons/industries-construction.svg",
-        title: "Contractor Compliance",
-        desc: "Ensure only compliant workers enter your site.",
-        href: "/contractor-compliance",
-      },
+      // {
+      //   icon: "/nav/icons/industries-construction.svg",
+      //   title: "Contractor Compliance",
+      //   desc: "Ensure only compliant workers enter your site.",
+      //   href: "/contractor-compliance",
+      // },
     ],
   },
 };
