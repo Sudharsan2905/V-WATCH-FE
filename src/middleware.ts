37ed5949e81f608a0d2ab43dde941735ae39056e
Sudharsan Senthil <sudharsan.softsuave@gmail.com>
2026-07-16 12:23:33 +0530

    Remove the maintenance page.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,14 @@ import type { NextRequest } from "next/server";
 const MAINTENANCE_PATH = "/maintenance";
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  if (pathname === MAINTENANCE_PATH) {
     return NextResponse.next();
-  }
+  // const { pathname } = request.nextUrl;
 
-  return NextResponse.redirect(new URL(MAINTENANCE_PATH, request.url));
+  // if (pathname === MAINTENANCE_PATH) {
+  //   return NextResponse.next();
+  // }
+
+  // return NextResponse.redirect(new URL(MAINTENANCE_PATH, request.url));
 }
 
 export const config = {
