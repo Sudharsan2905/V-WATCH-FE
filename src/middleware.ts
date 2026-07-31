9a23e19872acee58cdab0ade1501703811c74ec9
Sudharsan Senthil <sudharsan.softsuave@gmail.com>
2026-07-31 13:22:23 +0530

    Production Release test - 31-07

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const MAINTENANCE_PATH = "/maintenance";
+
+// const MAINTENANCE_PATH = "/maintenance";
 
 export function middleware(request: NextRequest) {
     return NextResponse.next();
